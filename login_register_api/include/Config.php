85aa74c11f34db1e7fbfab3b35c709e8a8679336
InzzPK <inzz.pk@gmail.com>
2015-10-11 09:50:51 +0700

    first commit

---
+++ login_register_api/include/Config.php
@@ -0,0 +1,13 @@
+<?php
+//Variables needed for database connection
+//define("DB_HOST", "localhost");
+//define("DB_USER", "root");
+//define("DB_PASSWORD", "");
+//define("DB_DATABASE", "login_api");
+
+define("DB_HOST", "localhost");
+define("DB_USER", "inzzpkinth_KUmap");
+define("DB_PASSWORD", "Ku123456");
+define("DB_DATABASE", "inzzpkinth_KUmap");
+//define("DB_NAME", "inzzpkinth_KUmap");
+?>
